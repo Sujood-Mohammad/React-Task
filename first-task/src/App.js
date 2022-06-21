0020852bac21b4fe73afa4945e1195bd7a599f7d
Sujood-Mohammad <sujoodmoh1996@gmail.com>
2022-06-21 15:41:47 +0300

    main

---
+++ first-task/src/App.js
@@ -0,0 +1,14 @@
+import Nav from './component/Nav';
+import Body from './component/Body';
+import Table from './component/Table';
+function App(){
+  return(
+    <div className='container'>
+       <Nav />
+       <Body />
+       <Table />
+    </div>
+  
+  );
+}
+export default App;
